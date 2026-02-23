30a3a038d9b96124278f3fadab120b7b2c17eab5
mani143gg <manikandan143g@gmail.com>
2026-02-24 00:15:59 +0530

    feat: Add an animated preloader component and integrate it into the application's initial load sequence.

---
+++ src/components/Loader.jsx
@@ -0,0 +1,19 @@
+import "../styles/loader.css"
+
+function Loader() {
+  return (
+    <div className="loader-container">
+      <div className="music-bars">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <h2 className="loader-text">Sreshtta Performing Arts</h2>
+    </div>
+  )
+}
+
+export default Loader
